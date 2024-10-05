c3fb8541c1c1a9e33b06a32f6ec2b4e5c73b7624
Rúni H. Hansen <runi.hansen@okkara.net>
2024-10-05 14:10:24 +0200

    Basic home-manager setup for MacOs

---
+++ home.nix
@@ -0,0 +1,120 @@
+{  pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home = {
+    username = "runih";
+    homeDirectory = "/Users/runih";
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.05"; # Please read the comment before changing.
+
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      dig
+      neovim
+      go
+      lazygit
+      nerdfonts
+      nodejs_22
+      pass
+      rustup
+      tmux
+      git
+      w3m
+    ];
+
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/runih/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+
+  };
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      git = true;
+      icons = true;
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    zsh = {
+      enable = true;
+      shellAliases = {
+        cd = "z";
+        cdi = "zi";
+      };
+    };
+    neovide = {
+      enable = true;
+      settings = {
+        fork = true;
+        frame = "full";
+        idle = true;
+        minimized = false;
+        neovim-bin = "/Users/runih/.nix-profile/bin/nvim";
+        no-multigrid = false;
+        srgb = false;
+        tabs = true;
+        theme = "auto";
+        title-hidden = true;
+        vsync = true;
+        wsl = false;
+
+        font = {
+          normal = [ "Terminess Nerd Font Propo"];
+          size = 10.0;
+          hinting = "full";
+          edging = "antialias";
+        };
+      };
+    };
+  };
+}
