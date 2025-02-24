98b5e2e924fbca08ab28faedbffee59c00c01ece
Rúni H. Hansen <runi.hansen@okkara.net>
2025-02-24 11:01:00 +0100

    Revert home.nix for my mac

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
       binutils
       btop
       dig
+      gimp
       git
       gnupg1
       go
@@ -34,6 +35,10 @@
       lazygit
       libjpeg
       neovim
+      nerd-fonts.hack
+      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
+      nerd-fonts.iosevka-term-slab
       ninja
       nodejs_22
       pass
@@ -118,7 +123,7 @@
     oh-my-posh = {
       enable = true;
       enableZshIntegration = true;
-      useTheme = "sorin";
+      useTheme = "the-unnamed";
     };
     zsh = {
       enable = true;
@@ -128,24 +133,24 @@
       };
     };
     wezterm = {
-      enable = false;
+      enable = true;
       colorSchemes = {
         myTheme = {
-          ansi = [
-            "#222222" "#D14949" "#48874F" "#AFA75A"
-            "#599797" "#8F6089" "#5C9FA8" "#8C8C8C"
-          ];
-          brights = [
-            "#444444" "#FF6D6D" "#89FF95" "#FFF484"
-            "#97DDFF" "#FDAAF2" "#85F5DA" "#E9E9E9"
-          ];
-          background = "#1B1B1B";
-          cursor_bg = "#BEAF8A";
-          cursor_border = "#BEAF8A";
-          cursor_fg = "#1B1B1B";
-          foreground = "#BEAF8A";
-          selection_bg = "#444444";
-          selection_fg = "#E9E9E9";
+            ansi = [
+                "#222222" "#D14949" "#48874F" "#AFA75A"
+              "#599797" "#8F6089" "#5C9FA8" "#8C8C8C"
+            ];
+            brights = [
+                "#444444" "#FF6D6D" "#89FF95" "#FFF484"
+              "#97DDFF" "#FDAAF2" "#85F5DA" "#E9E9E9"
+            ];
+            background = "#1B1B1B";
+            cursor_bg = "#BEAF8A";
+            cursor_border = "#BEAF8A";
+            cursor_fg = "#1B1B1B";
+            foreground = "#BEAF8A";
+            selection_bg = "#444444";
+            selection_fg = "#E9E9E9";
         };
       };
       enableZshIntegration = true;
@@ -172,13 +177,13 @@
       '';
     };
     neovide = {
-      enable = false;
+      enable = true;
       settings = {
         fork = true;
         frame = "full";
         idle = true;
         minimized = false;
-        neovim-bin = "/homeUsers/runih/.nix-profile/bin/nvim";
+        neovim-bin = "/Users/runih/.nix-profile/bin/nvim";
         no-multigrid = false;
         srgb = false;
         tabs = true;
