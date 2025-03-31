89bda6e363a6b5fd3385416725d436ca941307f6
Rúni H. Hansen <runi.hansen@okkara.net>
2025-03-31 17:42:51 +0200

    Changed to the latest NeoVim configuration

--- neovim.nix
+++ neovim.nix
@@ -3,8 +3,8 @@ let
   my-neovim-config = pkgs.fetchFromGitHub {
     owner = "runih";
     repo = "neovim-lua-config";
-    rev = "78751bdb1bd051ee31a84384b0a1c2f350c8ed33";
-    hash = "sha256-SKTfvvYkNGi4uE+3mKLAEIsh61v7gTGisw17Xln7+NM=";
+    rev = "69ecd455c2f407890f845c9507ffc80f8d5727da";
+    hash = "sha256-lhGHEGcGo5Qw8Iz4XRJvdaK/HLe8APFLBxFoq6DCUUk=";
   };
 in
   {
