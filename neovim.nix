6f94c3bdc0127638bc985ba9d7f1c14d34a274b7
Rúni H.Hansen <runi.hansen@okkara.net>
2025-09-29 22:31:27 +0200

    Updated flake and my neovim setup

--- neovim.nix
+++ neovim.nix
@@ -3,8 +3,8 @@ let
   my-neovim-config = pkgs.fetchFromGitHub {
     owner = "runih";
     repo = "neovim-lua-config";
-    rev = "aaff3b29344e964f780db9b2314289e44993cb82";
-    hash = "sha256-pOq2x5zAEulyDlSN2cvniNf3wRlkKOe44CfXi5zQ6YA=";
+    rev = "d49996e35941207d3f4db14081208a723bbb22c2";
+    hash = "sha256-ccMspTqY4wmb11PJKAa315dUOcehdj9cMFQZSQCFO8Q=";
   };
 in
   {
