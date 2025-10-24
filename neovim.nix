f79b1cdb800e7314901592959a531cc16b9a958e
Rúni H.Hansen <runi.hansen@okkara.net>
2025-10-24 22:33:05 +0200

    chore(neovim): update config repo revision and hash
    Updated the neovim-lua-config GitHub repository revision and hash to
    track the latest configuration changes.

--- neovim.nix
+++ neovim.nix
@@ -3,8 +3,8 @@ let
   my-neovim-config = pkgs.fetchFromGitHub {
     owner = "runih";
     repo = "neovim-lua-config";
-    rev = "d49996e35941207d3f4db14081208a723bbb22c2";
-    hash = "sha256-ccMspTqY4wmb11PJKAa315dUOcehdj9cMFQZSQCFO8Q=";
+    rev = "d080bd97a251dea8da89e35948f15709f8a0c952";
+    hash = "sha256-4aJRDraZFAxK9hClL9QegR4PSgtYA4695OUoq4B2ooA=";
   };
 in
   {
